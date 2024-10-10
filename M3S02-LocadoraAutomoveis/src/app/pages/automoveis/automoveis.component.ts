e9916960df8f1d997c13fd0f05089e09b96dc92b
Evelin Nunes <evelinlnunes@gmail.com>
2024-10-10 14:19:02 +0200

    [M3S02] Ex 04 - Desenvolvimento da Tela de Listagem

--- M3S02-LocadoraAutomoveis/src/app/pages/automoveis/automoveis.component.ts
+++ M3S02-LocadoraAutomoveis/src/app/pages/automoveis/automoveis.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { AutomoveisService } from '../../services/automoveis.service';
 
 @Component({
   selector: 'app-automoveis',
@@ -12,6 +13,20 @@ export class AutomoveisComponent {
   automoveisList: any = [];
   router = inject(Router);
 
+  automoveisService = inject(AutomoveisService);
+
+  ngOnInit(): void {
+    this.automoveisService.list().subscribe({
+      next: (data: any) => {
+        this.automoveisList = data;
+        console.log(this.automoveisList);
+      },
+      error: (error) => {
+        console.error('Erro ao listar pacientes: ', error);
+      },
+    });
+  }
+  
   redirectTo(id: number) {
     this.router.navigate(['/automoveis', id]);
   }
--- M3S02-LocadoraAutomoveis/src/app/services/automoveis.service.ts
+++ M3S02-LocadoraAutomoveis/src/app/services/automoveis.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutomoveisService {
 
+  httpClient = inject(HttpClient);
+  pathUrl = "https://6707b16d8e86a8d9e42c8435.mockapi.io/automoveis"
   constructor() { }
+
+  list() {
+    return this.httpClient.get(`${this.pathUrl}`);
+  }
+  detail(id: number) {
+    return this.httpClient.get(`${this.pathUrl}/${id}`);
+  }
 }
