b3dfd100be1562bd906766300b378ed959e7d3b6
Evelin Nunes <evelinlnunes@gmail.com>
2024-10-10 13:55:57 +0200

    [M3S02] Ex 03 - Desenvolvimento de Componentes

--- M3S02-LocadoraAutomoveis/src/app/pages/automoveis/automoveis.component.ts
+++ M3S02-LocadoraAutomoveis/src/app/pages/automoveis/automoveis.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-automoveis',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './automoveis.component.css'
 })
 export class AutomoveisComponent {
+  automoveisList: any = [];
+  router = inject(Router);
 
+  redirectTo(id: number) {
+    this.router.navigate(['/automoveis', id]);
+  }
 }
