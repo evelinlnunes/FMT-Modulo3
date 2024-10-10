3f28d5b85cc408d5f234104033b0640a04b8a18a
Evelin Nunes <evelinlnunes@gmail.com>
2024-10-10 15:27:59 +0200

    [M3S03] - Ex 06 - Testes de cálculo no componente

--- M3S03-Calculadora/src/app/calculadora/calculadora.component.spec.ts
+++ M3S03-Calculadora/src/app/calculadora/calculadora.component.spec.ts
@@ -20,4 +20,11 @@ describe('CalculadoraComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve adicionar 2 numeros e retornar a resposta correta', () => {
+    component.num1 = 12;
+    component.num2 = 20;
+    component.somar();
+    expect(component.result).toBe(32);
+  });
 });
