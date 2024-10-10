0f4c3a071e415ae07607ac5fe39e75a25c9c9b00
Evelin Nunes <evelinlnunes@gmail.com>
2024-10-10 13:21:54 +0200

    [M3S02] Ex 02 - Adaptação de Roteamento

--- M3S02-LocadoraAutomoveis/src/app/app.routes.ts
+++ M3S02-LocadoraAutomoveis/src/app/app.routes.ts
@@ -1,3 +1,26 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AutomoveisComponent } from './pages/automoveis/automoveis.component';
+import { AutomoveisDetailsComponent } from './pages/automoveis/automoveis-details/automoveis-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'automoveis',
+        children: [
+            {
+                path: '',
+                component: AutomoveisComponent,
+            },
+            {
+                path: ':id',
+                component: AutomoveisDetailsComponent
+            }
+        ]
+    },
+    {
+        path: '', redirectTo: '/automoveis', pathMatch: 'full'
+    },
+    {
+        path: '**', redirectTo: '/automoveis'
+    }
+];
