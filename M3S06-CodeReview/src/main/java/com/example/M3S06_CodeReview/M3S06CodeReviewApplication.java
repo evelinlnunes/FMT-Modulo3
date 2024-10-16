8d8a0fbcab86d3e0ea9d7f787b22e1723ad6201e
Evelin Nunes <evelinlnunes@gmail.com>
2024-10-16 12:03:22 +0200

    M3S06

---
+++ M3S06-CodeReview/src/main/java/com/example/M3S06_CodeReview/M3S06CodeReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.M3S06_CodeReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3S06CodeReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3S06CodeReviewApplication.class, args);
+	}
+
+}
---
+++ M3S06-CodeReview/src/test/java/com/example/M3S06_CodeReview/M3S06CodeReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.M3S06_CodeReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3S06CodeReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
