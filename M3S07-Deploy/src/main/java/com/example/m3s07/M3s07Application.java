f7b06465828b753b35e62ba09a9d14cdc23edc92
Evelin Nunes <evelinlnunes@gmail.com>
2024-11-06 16:22:00 +0100

    [M03S07] Ex 1 - Setup Projeto

---
+++ M3S07-Deploy/src/main/java/com/example/m3s07/M3s07Application.java
@@ -0,0 +1,13 @@
+package com.example.m3s07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3s07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3s07Application.class, args);
+	}
+
+}
---
+++ M3S07-Deploy/src/test/java/com/example/m3s07/M3s07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m3s07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3s07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
